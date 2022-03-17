13be1cf858be3c37a47cc5d300424e65b913001d
Salsabeel-tn <salsabeelj66@gmail.com>
2022-03-17 16:02:34 +0300

    updated user login section

--- FlaskMarket/market/models.py
+++ FlaskMarket/market/models.py
@@ -1,6 +1,11 @@
 from market import db
 from market import bcrypt
-class User(db.Model):
+from market import login_manager
+from flask_login import UserMixin
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(),primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_adress = db.Column(db.String(length=50), nullable=False, unique=True)
@@ -8,7 +13,7 @@ class User(db.Model):
     budget = db.Column(db.Integer(), nullable=False,default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
     
-    #creating this decorator to hash the received password
+    #creating this decorator to hash the received password and store it in password_hash column
     @property
     def password(self):
         return self.password
@@ -16,7 +21,11 @@ class User(db.Model):
     @password.setter
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+    
 
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+        
 class Item(db.Model):
     #creating db table structure
     id = db.Column(db.Integer(),primary_key=True)
--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -1,9 +1,9 @@
-from FlaskMarket.market.forms import LoginForm
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user
 @app.route("/")
 def home_page():
     return render_template('home.html')
@@ -33,8 +33,16 @@ def register_page():
 @app.route('/login', methods = ['GET', 'POST'])
 def login_page():
     form = LoginForm()
-
+    if form.validate_on_submit():
+        attempted_user = User.query.get(form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are Logged in as: {attempted_user.username}')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username or Password is not correct! please try again', category='danger')
     return render_template('login.html', form = form)
+
 
 
 
